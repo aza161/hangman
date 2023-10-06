9ad10447b18b39f815d1edf938f92f2e689b7f71
Ahmad <146203155+aza161@users.noreply.github.com>
2023-10-06 22:34:21 +0300

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 public class Hangman
 {
@@ -7,10 +7,21 @@ public class Hangman
         Scanner input = new Scanner (System.in);
         System.out.println
                 ("CMPS201 Hangman!\nI will think of a random word. You'll try to guess its\nletters. Every time you guess a letter that isn't in my\nword, a new body part of the hanging man appears.\nGuess correctly to avoid the gallows!\n");
-        String word = "PROGRAMMER";
-        Scanner file = new Scanner(new File("res/display8.txt"));
-        while (file.hasNext()){
-            System.out.println(file.nextLine());}
+        System.out.print ("Please choose a file (dict, large, small, oneword, twowords, wordlenghts): ");
+        String fname = input.next ();
+        Scanner dictionary = new Scanner (new File ("res/" + fname + ".txt"));
+        Random rand = new Random ();
+        String word = "";
+        int n = (rand.nextInt (dictionary.nextInt ()) + 1);
+        for (int i = 0; i < n; i++)
+        {
+            word = dictionary.next ();
+        }
+        Scanner file = new Scanner (new File ("res/display8.txt"));
+        while (file.hasNext ())
+        {
+            System.out.println (file.nextLine ());
+        }
         System.out.print ("Secret word : ");
         String hint = "";
         String new_hint = "";
@@ -23,70 +34,95 @@ public class Hangman
         String guesses = "";
         int guesses_left = 8;
         boolean correct;
+        input.nextLine ();
         System.out.print ("\nYour guesses: " + "\nGuesses left: " + guesses_left + "\nYour guess? ");
-        String guess = input.nextLine();
+        String guess = input.nextLine ();
         guess = guess.toUpperCase ();
-        do{
-        if (guess.length() > 1 || !Character.isLetter(guess.charAt(0))){
-            System.out.print("Type a single letter from A-Z.\nYour guess? ");
-            guess = input.nextLine();
-            guess = guess.toUpperCase ();
-        }
-        if (guesses.contains(guess)){
-            System.out.print("You already guessed that letter.\nYour guess? ");
-            guess = input.nextLine();
-            guess = guess.toUpperCase ();
-        }
+        do
+        {
+            while (guess.length () > 1 || !Character.isLetter (guess.charAt (0)))
+            {
+                System.out.print ("Type a single letter from A-Z.\nYour guess? ");
+                guess = input.nextLine ();
+                guess = guess.toUpperCase ();
+            }
+            while (guesses.contains (guess))
+            {
+                System.out.print ("You already guessed that letter.\nYour guess? ");
+                guess = input.nextLine ();
+                guess = guess.toUpperCase ();
+            }
             //guesses += guess;
 
-        if (word.contains (guess))
-        {
-            Scanner file1 = new Scanner(new File("res/display" + guesses_left + ".txt"));
-            while (file1.hasNext()){
-                System.out.println(file1.nextLine());
+            if (word.contains (guess))
+            {
+                Scanner file1 = new Scanner (new File ("res/display" + guesses_left + ".txt"));
+                while (file1.hasNext ())
+                {
+                    System.out.println (file1.nextLine ());
+                }
+                System.out.println ("Correct!\n");
+                num_guesses += 1;
+                correct = true;
             }
-            System.out.println ("Correct!\n");
-            num_guesses += 1;
-            correct = true;
-        }
-        else
-        {
-            Scanner file1 = new Scanner(new File("res/display" + (guesses_left -1 )+ ".txt"));
-            while (file1.hasNext()){
-                System.out.println(file1.nextLine());
+            else
+            {
+                Scanner file1 = new Scanner (new File ("res/display" + (guesses_left - 1) + ".txt"));
+                while (file1.hasNext ())
+                {
+                    System.out.println (file1.nextLine ());
+                }
+                System.out.println ("Incorrect!\n");
+                num_guesses += 1;
+                guesses_left -= 1;
+                correct = false;
             }
-            System.out.println ("Incorrect!\n");
-            num_guesses += 1;
-            guesses_left -= 1;
-            correct = false;
-        }
-        if (guess.length() == 1 && Character.isLetter(guess.charAt(0))){
-            guesses += guess;
-        if (correct)
-        {   new_hint = "";
-            for(int i = 0; i < hint.length(); i++){
-                if(word.charAt(i) == guess.charAt(0)){
-                    new_hint += word.charAt(i);
-                }else{
-                    new_hint += hint.charAt(i);
+            if (guess.length () == 1 && Character.isLetter (guess.charAt (0)))
+            {
+                guesses += guess;
+                if (correct)
+                {
+                    new_hint = "";
+                    for (int i = 0; i < hint.length (); i++)
+                    {
+                        if (word.charAt (i) == guess.charAt (0))
+                        {
+                            new_hint += word.charAt (i);
+                        }
+                        else
+                        {
+                            new_hint += hint.charAt (i);
+                        }
+                    }
+                    hint = new_hint;
+                }
+                if (guesses_left > 0 && !new_hint.equals (word))
+                {
+                    if (guesses_left == 7 && num_guesses == 1 || guesses_left == 6 && num_guesses == 2 || guesses_left == 5 && num_guesses == 3 || guesses_left == 4 && num_guesses == 4 || guesses_left == 3 && num_guesses == 5 || guesses_left == 2 && num_guesses == 6 || guesses_left == 1 && num_guesses == 7 || guesses_left == 0 && num_guesses == 8)
+                    {
+                        System.out.print ("Secret word : " + hint + "\n\nYour guesses: " + guesses + "\n\nGuesses left: " + guesses_left + "\nYour guess? ");
+                    }
+                    else
+                    {
+                        System.out.print ("Secret word : " + new_hint + "\n\nYour guesses: " + guesses + "\n\nGuesses left: " + guesses_left + "\nYour guess? ");
+                    }
+                    guess = input.nextLine ();
+                    guess = guess.toUpperCase ();
+                }
+                else if (guesses_left > 0 && new_hint.equals (word))
+                {
+                    System.out.print ("You win! My word was \"" + word + "\"");
+                }
+                else
+                {
+                    System.out.print ("You Lost!");
                 }
             }
-            hint = new_hint;
-        }if(guesses_left > 0 && !new_hint.equals(word))
-        {
-        System.out.print ("Secret word : " + new_hint + "\n\nYour guesses: " + guesses + "\n\nGuesses left: " + guesses_left + "\nYour guess? ");
-        guess = input.nextLine();
-        guess = guess.toUpperCase ();
-        }else if(guesses_left > 0 && new_hint.equals(word))
-        {
-        System.out.print("You win! My word was \"" + word + "\"");
-        }else{
-            System.out.print("You Lost!");
-            }
         }
-        }while(guesses_left > 0 && !new_hint.equals(word));
-        if (guesses_left == 0){
-            System.out.println("");
+        while (guesses_left > 0 && !new_hint.equals (word));
+        if (guesses_left == 0)
+        {
+            System.out.println ("");
         }
     }
 }
